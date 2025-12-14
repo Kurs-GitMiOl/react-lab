2097dcff92208bad52bf81093d399be7c5172773
student <mirek_olszowka_git@o2.pl>
2025-12-14 11:25:17 +0100

    kimit 3 11:25

--- src/App.js
+++ src/App.js
@@ -35,19 +35,35 @@ function App() {
     //let title = '1670';
     //title = event.target.value;
      const [title, setTitle] = useState('Wall-E'); //hook
+     //const [komunikat, setKomunikat] = useState("")
 
 
     function handleChange(event) {
         //console.log(event.target.value);
             setTitle(event.target.value);
-
     }
+
+    let message= '';
+    if (title.length < 5){
+        message = 'Title is too short';
+    }
+    else if (title.length  < 10)
+    {
+        message = 'Title is extra';
+    } else
+    {
+        message = 'Title to long ';
+    }
+
 // struct komponentu
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
             <h3>My favourite movie for today is {title}</h3>
+            <h3> {message}</h3>
+
+
             <input type="text" onChange={handleChange}/>
         </div>
     );
