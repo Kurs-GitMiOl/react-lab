5b8585fffae7a175a5a39822d1738f2a276f8edd
student <mirek_olszowka_git@o2.pl>
2025-12-14 11:06:03 +0100

    kimit 2 11:05

--- src/App.js
+++ src/App.js
@@ -29,17 +29,25 @@ import './App.css';
 //     );
 //
 // }
+
+import {useState} from "react";
 function App() {
-    let title = 'Wall-E';
+    //let title = '1670';
+    //title = event.target.value;
+     const [title, setTitle] = useState('Wall-E'); //hook
+
 
     function handleChange(event) {
-        console.log(event.target.value);
-    }
+        //console.log(event.target.value);
+            setTitle(event.target.value);
 
+    }
+// struct komponentu
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
+            <h3>My favourite movie for today is {title}</h3>
             <input type="text" onChange={handleChange}/>
         </div>
     );
