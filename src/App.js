366da0147c54b9afed01c87c1abbc20230f53be8
student <mirek_olszowka1@o2.pl>
2026-01-17 10:33:00 +0100

    comit10_30

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import "milligram";
 import {useState} from "react";
 
 function App() {
-    const [title, setTitle] = useState('Wall-E');
+     const [title, setTitle] = useState(''); // pierwszy etap
+     const [year, setYear] = useState(''); // pierwszy etap
+
+    const [movies, setMovies] = useState([]);
+
+    //const [year, setMovies] = useState([]);
+
+
 
     let message;
     if (title.length < 5) {
@@ -18,12 +25,27 @@ function App() {
         setTitle(event.target.value);
     }
 
-    const movies = [
-        {title: "Wall-E"},
-        {title: "Pulp Fiction"},
-        {title: "Matrix"},
-        {title: "1670"},
-    ];
+    // const movies = [
+    //     {title: "Wall-E"},
+    //     {title: "Pulp Fiction"},
+    //     {title: "Matrix"},
+    //     {title: "1670"},
+    // ];
+
+    function handleAddmovie(){
+        const  movie = {
+            title: title,
+            year: year,
+        };
+        //console.log(movie)
+        //movies.push(movie);
+        //setMovies(movies)
+
+        setMovies([...movies, movie]);
+        setTitle('')
+        setYear('')
+
+    }
 
     return (
         <div className="container">
@@ -31,14 +53,28 @@ function App() {
             <h1>My favourite movies to watch</h1>
             <h2>Titles</h2>
             <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title}</li>)}
+                {movies.map(movie => <li key={movie.title}>{movie.title} {movie.year}</li>)}
             </ul>
-            <h2>My favourite movie for today is {title}</h2>
-            {title.length > 0 && <div>{message}</div>}
-            <input type="text" value={title} onChange={handleChange}/>
-            <button onClick={() => alert(title)}>Pokaż tytuł filmu</button>
+            <h2>Add movie</h2>
+            <label>Tytuł</label>
+            <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
+
+
+            <label>Rok wydania {year}</label>
+            <input type="text" value={year} onChange={(event) => setYear(event.target.value)}/>
+
+
+            {/*{title.length > 0 && <div>{message}</div>}*/}
+            {/*{title.length > 0 && <div>{message}</div>}*/}
+
+            {/*<input type="text" value={title} onChange={handleChange}/>*/}
+            {/*<button onClick={() => alert(title)}>Dodaj film</button>*/}
+
+            <button onClick={handleAddmovie}>Dodaj film</button>
+
         </div>
     );
 }
 
 export default App;
+
